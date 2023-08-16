e98fdbb04db0861d5cb0eb7115c53f127cfd9cae
Claire Dimitriuc <121368271+macaroonforu@users.noreply.github.com>
2023-08-15 21:19:10 -0400

    Update HeaderForm.jsx to have field limits

--- src/components/HeaderForm.jsx
+++ src/components/HeaderForm.jsx
@@ -11,22 +11,22 @@ function HeaderForm({formData, handleChange}) {
 
         <div className="headerRow">
         <label htmlFor="name">Name:</label>
-        <input className="headerFormInput" type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+        <input className="headerFormInput" type="text" maxLength="35" id="name" name="name" value={formData.name} onChange={handleChange}/>
         </div>
 
         <div className="headerRow">
         <label htmlFor="email">Email:</label>
-        <input className="headerFormInput" type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
+        <input className="headerFormInput" type="email" maxLength="35" id="email" name="email" value={formData.email} onChange={handleChange}/>
         </div>
         
         <div className="headerRow">
         <label htmlFor="number">Phone Number:</label>
-        <input className="headerFormInput" type="message" id="message" name="number" value={formData.number} onChange={handleChange}/>
+        <input className="headerFormInput" type="message" maxLength="15" id="message" name="number" value={formData.number} onChange={handleChange}/>
         </div>
 
         <div className="headerRow">
         <label htmlFor="location">Location</label>
-        <input className="headerFormInput" type="location" id="location" name="city" value={formData.city} onChange={handleChange}/>
+        <input className="headerFormInput" type="location" maxLength="25" id="location" name="city" value={formData.city} onChange={handleChange}/>
         </div>
         </div>
         
